f05e6f5bc210a4af2b241b3ce3e36da3506e0ff0
Tzu Huan Tai <tzu.huan.tai@gmail.com>
2023-01-07 21:23:13 -0800

    fix: nav switch on/off

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit{
+export class NavComponent implements OnInit {
 
   isMenuCollapsed: boolean = true;
 
   ngOnInit(): void {
-    
+
+  }
+
+  switchNav(): void {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
+
+  closeNav(): void {
+    this.isMenuCollapsed = true;
   }
 }
